75650423d7fc8b49f8ac08034cde27562a9a3821
JhonyCanella <73204467+jcanella@users.noreply.github.com>
2026-02-04 23:14:59 -0300

    Delete GerenciadorDeGastos directory

--- GerenciadorDeGastos/insights.py
+++
@@ -1,9 +0,0 @@
-
-def obter_salario(sheet):
-    try:
-        salario = sheet.acell("B1").value
-        if not salario:
-            return None
-        return float(salario.replace(",", "."))
-    except:
-        return None
--- GerenciadorDeGastos/router.py
+++
@@ -1,9 +0,0 @@
-from config import USUARIOS
-from sheets import get_sheet
-
-def get_user_sheet(user_id, aba="GASTOS"):
-    if user_id not in USUARIOS:
-        raise PermissionError("Usuário não autorizado")
-
-    spreadsheet = USUARIOS[user_id]["spreadsheet"]
-    return get_sheet(aba, spreadsheet)
