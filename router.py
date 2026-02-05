b00fcd2ff11763cf588e2b9cd442b3671dcf6ea3
JhonyCanella <73204467+jcanella@users.noreply.github.com>
2026-02-04 23:35:49 -0300

    Update router.py

--- router.py
+++ router.py
@@ -1,9 +1,9 @@
-from config import USUARIOS
+from settings import USUARIOS
 from sheets import get_sheet
 
 def get_user_sheet(user_id, aba="GASTOS"):
     if user_id not in USUARIOS:
         raise PermissionError("Usuário não autorizado")
 
-    spreadsheet = USUARIOS[user_id]["spreadsheet"]
+    spreadsheet = USUARIOS[str(user_id)]["spreadsheet"]
     return get_sheet(aba, spreadsheet)
