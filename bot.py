e854aebadbdc19adc8f043c96d3053e6a77ce5e8
JhonyCanella <73204467+jcanella@users.noreply.github.com>
2026-02-05 00:55:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,12 @@
 import os
-import uuid
-from datetime import datetime
-
-from telegram.ext import (
-    ApplicationBuilder,
-    MessageHandler,
-    CommandHandler,
-    filters
-)
-
-from commands import (
-    help_cmd,
-    resumo_cmd,
-    quem_cmd,
-    insights_cmd,
-    add_beneficiario_cmd,
-    set_entrada_cmd,
-    start_cmd
-)
-
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters
+from commands import help_cmd, resumo_cmd, quem_cmd, insights_cmd , add_beneficiario_cmd, set_entrada_cmd, start_cmd
 from parser import extrair_data, extrair_valor, limpar_texto
 from categories import classificar
+import uuid
+from datetime import datetime
 from beneficiarios import listar_beneficiarios, beneficiario_valido
 from sheets import get_sheet
-
-
-# ===============================
-# CONFIG
-# ===============================
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
@@ -35,13 +14,9 @@ if not TELEGRAM_TOKEN:
     raise Exception("❌ TELEGRAM_TOKEN não configurado nas variáveis de ambiente")
 
 
-# ===============================
-# HANDLER PRINCIPAL
-# ===============================
-
 async def handler(update, context):
     texto = update.message.text.strip()
-    chat_id = str(update.effective_user.id)
+    chat_id = update.effective_user.id
 
     if texto.startswith("/"):
         return
@@ -68,10 +43,12 @@ async def handler(update, context):
         if not beneficiario_valido(beneficiario, chat_id):
             raise Exception(f"Beneficiário '{beneficiario}' não cadastrado")
 
+
         categoria = classificar(descricao)
 
+        chat_id = update.effective_chat.id
         sheet = get_sheet("GASTOS", chat_id)
-
+        
         sheet.append_row([
             str(uuid.uuid4()),
             data.strftime("%Y-%m-%d"),
@@ -91,16 +68,13 @@ async def handler(update, context):
             f"📝 {descricao}"
         )
 
+
     except Exception as e:
         await update.message.reply_text(f"❌ Erro ao interpretar: {e}")
 
 
-# ===============================
-# APP
-# ===============================
-
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handler))
 app.add_handler(CommandHandler("start", start_cmd))
 app.add_handler(CommandHandler("help", help_cmd))
 app.add_handler(CommandHandler("resumo", resumo_cmd))
@@ -108,7 +82,5 @@ app.add_handler(CommandHandler("quem", quem_cmd))
 app.add_handler(CommandHandler("insights", insights_cmd))
 app.add_handler(CommandHandler("beneficiario", add_beneficiario_cmd))
 app.add_handler(CommandHandler("entrada", set_entrada_cmd))
-
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handler))
 
 app.run_polling()
